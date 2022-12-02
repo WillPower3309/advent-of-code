83d4d8a9c22111074b4087a635720d6be0dac447
William McKinnon <contact@willmckinnon.com>
2022-12-02 00:30:41 -0500

    added day1.py

---
+++ day1.py
@@ -0,0 +1,16 @@
+# not perfectly optimal but certainly very pythonic :)
+
+buffer = []
+with open('input/day1.txt', 'r') as f:
+    buffer = f.read().split("\n\n")
+
+totalCalorieList = []
+for elf in buffer:
+    calorieList = (list(filter(None, elf.split('\n'))))
+    totalCalorieList.append(sum([int(x) for x in calorieList]))
+totalCalorieList.sort(reverse=True)
+
+# PART ONE
+print("The elf carrying the most calories is carrying %d" % totalCalorieList[0])
+# PART TWO
+print("The elf carrying the most calories is carrying %d" % sum(totalCalorieList[0:3]))
