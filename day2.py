d903322a0bb48fbae8ee82773c6bb886b873bd3a
William McKinnon <contact@willmckinnon.com>
2022-12-02 14:50:13 -0500

    made day2 more optimal

--- day2.py
+++ day2.py
@@ -1,24 +1,23 @@
-responseScoreDict = { 'X': 1, 'Y': 2, 'Z': 3 }
+# convert A/B/C to 1/2/3
+def abcToInt(move):
+    return ord(move) - 64
+
+# convert X/Y/Z to 1/2/3
+def xyzToInt(move):
+    return ord(move) - 87
 
 ##################
 # PART ONE
 ##################
 
-outcomeDict = {
-    'A': { 'X': 3, 'Y': 6, 'Z': 0 },
-    'B': { 'X': 0, 'Y': 3, 'Z': 6 },
-    'C': { 'X': 6, 'Y': 0, 'Z': 3 },
-}
-
-f = open('input/day2.txt', 'r')
+outcomeArr = [3, 0, 6]
 
 totalScore = 0
-buffer = f.readline()
-while buffer:
-    challenge = buffer[0]
-    response = buffer[2]
-    totalScore += responseScoreDict[response] + outcomeDict[challenge][response]
-    buffer = f.readline()
+with open('input/day2.txt', 'r') as f:
+    for line in f:
+        challenge = abcToInt(line[0])
+        response = xyzToInt(line[2])
+        totalScore += response + outcomeArr[(challenge - response) % 3]
 
 print("the score according to the strategy guide would be %d" % totalScore)
 
@@ -26,22 +25,19 @@ print("the score according to the strategy guide would be %d" % totalScore)
 # PART TWO
 ##################
 
-f = open('input/day2.txt', 'r')
-
-responseDict = {
-    'A': { 'X': 'Z', 'Y': 'X', 'Z': 'Y' },
-    'B': { 'X': 'X', 'Y': 'Y', 'Z': 'Z' },
-    'C': { 'X': 'Y', 'Y': 'Z', 'Z': 'X' },
-}
-outcomeDict = { 'X': 0, 'Y': 3, 'Z': 6 }
+def outcomeToResponse(challenge, outcome):
+    if outcome == 3: # tie
+        return challenge
+    elif outcome == 0: # loss
+        return (challenge + 1) % 3 + 1
+    elif outcome == 6: # win
+        return (challenge) % 3 + 1
 
 totalScore = 0
-buffer = f.readline()
-while buffer:
-    challenge = buffer[0]
-    outcome = buffer[2]
-    response = responseDict[challenge][outcome]
-    totalScore += responseScoreDict[response] + outcomeDict[outcome]
-    buffer = f.readline()
+with open('input/day2.txt', 'r') as f:
+    for line in f:
+        challenge = abcToInt(line[0])
+        outcome = (xyzToInt(line[2]) - 1) * 3
+        totalScore += outcome + outcomeToResponse(challenge, outcome)
 
 print("the score according to the strategy guide would be %d" % totalScore)
