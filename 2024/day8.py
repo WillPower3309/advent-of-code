eea48284709c904eb7f5d2f080bf6be10b6b3ccb
William McKinnon <contact@willmckinnon.com>
2024-12-10 01:51:44 -0500

    add day 8 part two

--- 2024/day8.py
+++ 2024/day8.py
@@ -1,4 +1,3 @@
-# PART ONE
 from collections import defaultdict
 
 buffer = []
@@ -13,6 +12,8 @@ for y in range(len(buffer)):
         if char != '.' and char != '\n':
             antennaCoordinateMap[char].append([y, x])
 
+# PART ONE
+
 antiNodeCoordinates = set()
 
 for antennaCoordList in antennaCoordinateMap.values():
@@ -24,8 +25,30 @@ for antennaCoordList in antennaCoordinateMap.values():
             antiNodeY = 2 * coordOne[0] - coordTwo[0]
             if antiNodeY < len(buffer) and antiNodeY >= 0:
                 antiNodeX = 2 * coordOne[1] - coordTwo[1]
-                if (antiNodeX < len(buffer[0]) and antiNodeX >= 0):
+                if antiNodeX < len(buffer[0]) and antiNodeX >= 0:
                     antiNodeCoordinates.add((antiNodeY, antiNodeX))
 
 print('PART 1: %d' % len(antiNodeCoordinates))
+
+
+# PART TWO
+
+antiNodeCoordinates = set()
+
+for antennaCoordList in antennaCoordinateMap.values():
+    for coordOne in antennaCoordList:
+        for coordTwo in antennaCoordList:
+            if coordOne == coordTwo:
+                continue
+
+            antiNodeCoordinates.add((coordOne[0], coordOne[1]))
+
+            antiNodeY = 2 * coordOne[0] - coordTwo[0]
+            antiNodeX = 2 * coordOne[1] - coordTwo[1]
+            while antiNodeY < len(buffer) and antiNodeY >= 0 and antiNodeX < len(buffer[0]) and antiNodeX >= 0:
+                antiNodeCoordinates.add((antiNodeY, antiNodeX))
+                antiNodeY -= coordTwo[0] - coordOne[0]
+                antiNodeX -= coordTwo[1] - coordOne[1]
+
+print('PART 2: %d' % len(antiNodeCoordinates))
 
