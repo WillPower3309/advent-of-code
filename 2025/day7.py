eba428545337a7f0a1ae899878f6831e6d9c61ba
William McKinnon <contact@willmckinnon.com>
2025-12-08 00:08:00 -0500

    added day 7 part two

--- 2025/day7.py
+++ 2025/day7.py
@@ -3,16 +3,16 @@ grid = []
 with open('input/day7.txt', 'r') as f:
     grid = f.read().splitlines()
 
-# PART ONE
-
-x = 0
+startX = 0
 for i in range(len(grid[0])):
     if grid[0][i] == 'S':
-        x = i
+        startX = i
         break
 
+# PART ONE
+
 checkedPositions = set()
-positions = [ (0, x) ]
+positions = [ (0, startX) ]
 numSplits = 0
 while positions:
     y, x = positions.pop()
@@ -30,6 +30,23 @@ while positions:
 
     checkedPositions.add((y, x))
 
-
 print('PART 1: %d' % numSplits)
+
+# PART TWO
+
+cache = dict()
+def dfs(y: int, x: int) -> int:
+    if x < 0 or x >= len(grid[0]):
+        return 0
+    if y >= len(grid):
+        return 1
+    if (y, x) in cache:
+        return cache[(y, x)]
+    if grid[y][x] == '^':
+        cache[(y, x)] = dfs(y + 1, x + 1) + dfs(y + 1, x - 1)
+        return cache[(y, x)]
+
+    return dfs(y + 1, x)
+
+print('PART 2: %d' % dfs(0, startX))
 
